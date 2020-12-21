f345b1d9ccf3ce211ed30a9ab99c22ca01730e21
Melbin Thomas <melbin@Melbins-MacBook-Pro.local>
2020-12-21 11:13:20 +0000

    Home page

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAGO6v5M_uKjCF9ufjQ5Sa65wKNWZu-05U",
+    authDomain: "clone-bb373.firebaseapp.com",
+    projectId: "clone-bb373",
+    storageBucket: "clone-bb373.appspot.com",
+    messagingSenderId: "954017907831",
+    appId: "1:954017907831:web:34fe64f5516745c6137272"
+  };
