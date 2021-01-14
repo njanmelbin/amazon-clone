7c1519583511205e2415ce857994e1b6de871207
Melbin Thomas <melbin@Melbins-MacBook-Pro.local>
2021-01-14 08:03:02 +0000

    Orders Page added

--- src/CheckoutProduct.js
+++ src/CheckoutProduct.js
@@ -1,36 +1,38 @@
-import React from 'react'
+import React from 'react';
 import './CheckoutProduct.css'
-import { useStateValue } from './StateProvider';
+import { useStateValue } from "./StateProvider";
 
-function CheckoutProduct({id,title,image,price,rating}) {
-    const [{basket} , dispatch ] = useStateValue();
+function CheckoutProduct({ id, image, title, price, rating, hideButton }) {
+    const [{ basket }, dispatch] = useStateValue();
 
     const removeFromBasket = () => {
-        // remove the item from basket
+        // remove the item from the basket
         dispatch({
-            type:'REMOVE_FROM_BASKET',
-            id:id,       
+            type: 'REMOVE_FROM_BASKET',
+            id: id,
         })
     }
 
     return (
-        <div className="checkoutProduct">
-            <img className= "checkoutProduct__image"src={image} alt=""/>
+        <div className='checkoutProduct'>
+            <img className='checkoutProduct__image' src={image} />
 
-            <div className="checkoutProduct__info">
-                <p className="checkoutProduct__title">{title}</p>
+            <div className='checkoutProduct__info'>
+                <p className='checkoutProduct__title'>{title}</p>
                 <p className="checkoutProduct__price">
-                    <small>{price}</small>
+                    <small>$</small>
+                    <strong>{price}</strong>
                 </p>
                 <div className="checkoutProduct__rating">
                     {Array(rating)
                     .fill()
-                    .map((_,i) =>(
+                    .map((_, i) => (
                         <p>🌟</p>
-                    ))
-                    }
-                </div>  
-                <button onClick={removeFromBasket}>Remove from Basket</button> 
+                    ))}
+                </div>
+                {!hideButton && (
+                    <button onClick={removeFromBasket}>Remove from Basket</button>
+                )}
             </div>
         </div>
     )
